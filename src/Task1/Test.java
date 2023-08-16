ab7fdbf5ad4a6619b62c4eb9e191ca42c6120350
Artem1548 <140644935+Artem1548@users.noreply.github.com>
2023-08-17 02:31:57 +0300

    Add files via upload

---
+++ src/Task1/Test.java
@@ -0,0 +1,7 @@
+package Task1;
+
+public class Test {
+    public static void main(String[] args) {
+        Timer.startTimer();
+    }
+}
---
+++ src/Task2/FizzBuzz.java
@@ -0,0 +1,69 @@
+package Task2;
+
+public class FizzBuzz {
+
+    private int n;
+    private int currentN;
+
+    public FizzBuzz(int n) {
+        this.n = n;
+        this.currentN = 1;
+    }
+
+    private void incrementAndNotify(){
+        currentN++;
+        notifyAll();
+    }
+
+    private void makeWait(){
+        try {
+            wait();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public synchronized void fizz(){
+        while(currentN <= n){
+            if(currentN % 3 == 0 && currentN % 5 != 0){
+                System.out.println("fizz");
+                incrementAndNotify();
+            } else {
+                makeWait();
+            }
+        }
+    }
+
+    public synchronized void buzz(){
+        while(currentN <= n){
+            if(currentN % 5 == 0 && currentN % 3 != 0){
+                System.out.println("buzz");
+                incrementAndNotify();
+            } else {
+                makeWait();
+            }
+        }
+    }
+
+    public synchronized void fizzBuzz(){
+        while(currentN <= n){
+            if(currentN % 5 == 0 && currentN % 3 == 0){
+                System.out.println("fizzBuzz");
+                incrementAndNotify();
+            } else {
+                makeWait();
+            }
+        }
+    }
+
+    public synchronized void notFizzBuzz(){
+        while(currentN <= n){
+            if(currentN % 5 != 0 && currentN % 3 != 0){
+                System.out.println(currentN);
+                incrementAndNotify();
+            } else {
+                makeWait();
+            }
+        }
+    }
+}
